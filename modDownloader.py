0b94ffbab09d7eb1b8fe099250198e8c7030cbe9
Sanath Jathanna <sanathjathanna@gmail.com>
2021-06-06 00:03:26 +0530

    change: modDownloader.py to a simple downloading script modDownloader.py downloads every link from links.csv

--- modDownloader.py
+++ modDownloader.py
@@ -1,11 +1,30 @@
-from html.parser import HTMLParser
+import wget
+import csv
+import os
+import requests
+import time
 
-linkList = []
+def downloadFile(url):
+    r = requests.get(url)
+    r.raise_for_status()
+    print(r.status_code, wget.filename_from_url(url))
+    with open("./files/" + wget.filename_from_url(url), "wb") as file:
+        file.write(r.content)
 
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        global linkList
-        if tag == 'a':
-            linkList += [x[1] for _, x in enumerate(attrs) if x[0] == 'href']
 
-parser = MyHTMLParser()
+print(f"started at {time.strftime('%X')}")
+
+try:
+    os.mkdir("./files")
+except FileExistsError:
+    pass
+
+with open("links.csv") as csvfile:
+    reader = csv.reader(csvfile)
+    i = 0
+    for line in reader:
+        print(i, end=' ')
+        downloadFile(line[0])
+        i = i + 1
+
+print(f"finished at {time.strftime('%X')}")
