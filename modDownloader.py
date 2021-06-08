022f7f9d1eab2e2611a00be78abeb2ea7d9f1d1b
Sanath Jathanna <sanathjathanna@gmail.com>
2021-06-08 21:39:20 +0530

    style: reformat with black

--- modDownloader.py
+++ modDownloader.py
@@ -22,7 +22,7 @@ async def download_partial(url, session, start, end):
     Returns:
         bytes: chunk of a file
     """
-    headers = {'Range': f'bytes={start}-{end}'}
+    headers = {"Range": f"bytes={start}-{end}"}
     async with session.get(url=url, headers=headers) as response:
         return await response.read()
 
@@ -45,22 +45,32 @@ async def download_file(pair, session):
         chunk_size = 3000000
 
         async with session.head(url=pair[1]) as response:
-            length = response.headers['content-length']
+            length = response.headers["content-length"]
 
-        res = await asyncio.gather(*[download_partial(pair[1], session, i * chunk_size, (i + 1) * chunk_size - 1) for i in range(0, ceil(int(length) / chunk_size))])
+        res = await asyncio.gather(
+            *[
+                download_partial(
+                    pair[1], session, i * chunk_size, (i + 1) * chunk_size - 1
+                )
+                for i in range(0, ceil(int(length) / chunk_size))
+            ]
+        )
 
-        async with async_open(file_path, 'wb') as file:
-            await file.write(b''.join(res))
-            print(pair[0], response.status, pair[1], sep='\t')
+        async with async_open(file_path, "wb") as file:
+            await file.write(b"".join(res))
+            print(pair[0], response.status, pair[1], sep="\t")
 
     except BaseException:
         print(traceback.format_exc(), pair[1])
 
 
 async def main(pairs):
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=None)) as session:
+    async with aiohttp.ClientSession(
+        connector=aiohttp.TCPConnector(limit=None)
+    ) as session:
         ret = await asyncio.gather(*[download_file(pair, session) for pair in pairs])
         print(f"Completed all {len(ret)} async calls")
+
 
 start_time = time.time()
 
